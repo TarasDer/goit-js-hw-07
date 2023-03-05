c0d6a44da01893e0c0c9db9cffa2176adac3fb96
TarasDer <tderev.switch@gmail.com>
2023-03-06 00:17:13 +0200

    Add first task code

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryEl.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src = ${event.target.dataset.source} width="800" height="600">
+`);
+
+  instance.show();
+});
+
+// console.log(galleryItems);
